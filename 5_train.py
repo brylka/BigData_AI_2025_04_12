edf0159c972422735982de799d165d47e2302d24
Bartosz Bryniarski <brylka@jelenia.com>
2025-04-13 12:46:34 +0200

    Niedziela

--- 5_train.py
+++ 5_train.py
@@ -1,3 +1,4 @@
+import joblib
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
@@ -59,36 +60,8 @@ mse = tf.keras.losses.MeanSquaredError()
 mse_value = mse(y_test, y_pred).numpy()
 print(f'Mean squared error: {mse_value}')
 
-# Nowa nieruchomość, dla której chcemy oszacować cenę
-new_property = {
-    'MedInc': 8.32,
-    'HouseAge': 25,
-    'AveRooms': 6.23,
-    'AveBedrms': 1.01,
-    'Population': 1800,
-    'AveOccup': 3.5,
-    'Latitude': 37.88,
-    'Longitude': -122.23
-}
+# Zapisa modelu do pliku
+model.save('model.keras')
 
-# Tworzenie DataFrame z nowymi danymi
-new_property_df = pd.DataFrame([new_property])
-
-# Skalowanie danych wejściowych przy użyciu tego samego skalera użytego do trenowania modelu
-scaled_new_property = scaler.transform(new_property_df)
-
-# Prognozowanie ceny przy użyciu wytrenowanego modelu
-predicted_price = model.predict(scaled_new_property)
-
-# Wypisanie prognozowanej ceny
-print(f'Prognozowana cena nieruchomości: {predicted_price[0][0]:.3f} (w jednostkach 100.000 USD)')
-
-# Obliczenie wartości nieruchomości
-property_value = predicted_price[0][0] * 100000
-# Formatowanie wartości z separatorem
-formatted_value = format(property_value, ',.0f').replace(',', '.')
-# Wydrukowanie prognozowanej ceny nieruchomości z separatorem
-print(f'Prognozowana cena nieruchomości: {formatted_value} dolarów.')
-
-# Informacja o zapisanym wykresie
-print("\nWykres funkcji straty został zapisany jako 'loss_plot.png'.")
+# Zapis scalera do pliku
+joblib.dump(scaler, 'scaler.pkl')
