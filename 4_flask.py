2795d1e8c219feab6ff26afed241cd8a395f23d8
Bartosz Bryniarski <brylka@jelenia.com>
2025-04-13 10:42:22 +0200

    Niedziela

--- 4_flask.py
+++ 4_flask.py
@@ -3,8 +3,13 @@ import joblib
 
 app = Flask(__name__)
 
+model = joblib.load('mnist_model.pkl')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    # logika rozpoznawania cyfr
+
     return render_template('digit.html')
 
 if __name__ == '__main__':
