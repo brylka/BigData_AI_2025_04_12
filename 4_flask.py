b86ff766d554c1c8a4f0c6e153bf64b99dd59765
Bartosz Bryniarski <brylka@jelenia.com>
2025-04-13 10:58:43 +0200

    Niedziela

--- 4_flask.py
+++ 4_flask.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import numpy as np
+from flask import Flask, render_template, request
+from PIL import Image
 import joblib
 
 app = Flask(__name__)
@@ -7,8 +9,15 @@ model = joblib.load('mnist_model.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == ('POST'):
+        file = request.files['file'] # Odbieramy plik
 
-    # logika rozpoznawania cyfr
+        img = Image.open(file).convert('L') # Konwertujemy obraz na skalę szarości
+        img = img.resize((28, 28)) # Zmieniamy rozmiar do 28x28 pikseli
+
+        img_array = np.array(img) # Przekształcamy obraz do wektora
+
+        print(img_array)
 
     return render_template('digit.html')
 
